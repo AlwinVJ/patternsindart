7960f2e1d88d6da43806c474707205372b7e84f8
Alwin VJ <alwinvj5@gmail.com>
2022-12-04 22:38:54 +0530

    Last commit on 4th Dec

---
+++ bin/kalpha1.dart
@@ -0,0 +1,20 @@
+// This program print K shaped alpha pattern
+
+import 'dart:io';
+
+void main(){
+  for(int i = 10; i>1; i--){
+    int k = 65;
+    for(int j = (i-1); j>=1;j--){
+        stdout.write("${String.fromCharCode(k++)} ");
+    }
+  stdout.writeln();
+  }
+  for(int i = 1; i<=5; i++){
+    int k= 65;
+    for(int j = 1; j<=i; j++){
+      stdout.write('${String.fromCharCode(k++)} ');
+    }
+    print('');
+  }
+}
